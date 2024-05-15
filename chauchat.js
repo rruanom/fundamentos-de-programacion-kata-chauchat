264076762e955f4fc2c97ea6147176e3f1608d28
(Robi) <(r.ruano3@gmail.com)>
2024-05-15 15:46:38 +0200

    Parte del encasquillamiento hecho

---
+++ chauchat.js
@@ -0,0 +1,14 @@
+
+let cargador = ["pium!", "pium!", "pium!", "pium!", "pium!", "pium!", "pium!"];
+function chauchat(carga){
+    for (let i = 0; i < carga.length; i++){
+        let numeroRandom = Math.random()
+        if(numeroRandom > 0.80){
+            i = carga.length + 1;
+            return "Illo, me he quedao pillá!"
+        } else {
+            console.log(cargador[i])
+        }
+    }
+};
+
